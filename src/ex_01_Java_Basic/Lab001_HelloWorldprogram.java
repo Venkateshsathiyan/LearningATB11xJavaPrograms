1aee13921a8dbf5885ae4df014beba81bfee8b13
venka <venkateshsathiyan@gmail.com>
2025-03-08 11:14:13 +0530

    07-03-2025- Hello code pushing.

--- src/ex_01_Java_Basic/Lab001_HelloWorldprogram.java
+++ src/ex_01_Java_Basic/Lab001_HelloWorldprogram.java
@@ -3,5 +3,6 @@ package ex_01_Java_Basic;
 public class Lab001_HelloWorldprogram {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Hello Robot");
     }
 }
