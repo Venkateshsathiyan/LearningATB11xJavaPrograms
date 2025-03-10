dbda81420c1a1013a95cf377abc170f498db57e3
venka <venkateshsathiyan@gmail.com>
2025-03-10 09:13:41 +0530

    10-03-25-commit Arithmetic_fns

---
+++ src/ex_01_Java_Basic/Arithmetic_fns.java
@@ -0,0 +1,18 @@
+package ex_01_Java_Basic;
+
+public class Arithmetic_fns {
+    public static void main(String[] args) {
+//        create dublicate -ctrl+d
+        System.out.println(2+2);
+        System.out.println(50-35);
+        System.out.println(100*100);
+        System.out.println(50/8);//sout gives int values not decimals
+        System.out.println("Hello everyone");
+        System.out.println("Hello Sir");//println print everything in new line
+        System.out.print("Good Morning"+"\s");// print stmt pr
+        //\s-space
+//        \t-tab
+        // int everything in single line
+        System.out.println("Have a nice day");
+    }
+}
---
+++ src/ex_01_Java_Basic/Comments_Java.java
@@ -0,0 +1,13 @@
+package ex_01_Java_Basic;
+
+public class Comments_Java {
+    public static void main(String[] args) {
+//    this is single comment-ctrl+/
+        System.out.println("This is single comment");
+        /**this is multiple comments
+         *generally comment line are ignored by java
+         * short cut- /** and enter
+         */
+        System.out.println("This is multi comment");
+     }
+}
