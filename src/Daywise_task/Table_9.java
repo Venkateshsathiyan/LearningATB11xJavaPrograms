2394299509e2615bf7e6f1cfef1a13f4a7fdb7b9
venka <venkateshsathiyan@gmail.com>
2025-03-11 08:46:45 +0530

    11-03-25-commit 9th table

---
+++ src/Daywise_task/Table_9.java
@@ -0,0 +1,18 @@
+package Daywise_task;
+
+public class Table_9 {
+    public static void main(String[] args) {
+       int a=9,b=1;
+        System.out.printf("%d*%d=%d%n",a,b,(a*b));b++;
+        System.out.printf("%d*%d=%d%n",a,b,(a*b));b++;
+        System.out.printf("%d*%d=%d%n",a,b,(a*b));b++;
+        System.out.printf("%d*%d=%d%n",a,b,(a*b));b++;
+        System.out.printf("%d*%d=%d%n",a,b,(a*b));b++;
+        System.out.printf("%d*%d=%d%n",a,b,(a*b));b++;
+        System.out.printf("%d*%d=%d%n",a,b,(a*b));b++;
+        System.out.printf("%d*%d=%d%n",a,b,(a*b));b++;
+        System.out.printf("%d*%d=%d%n",a,b,(a*b));b++;
+        System.out.printf("%d*%d=%d%n",a,b,(a*b));b++;
+
+        }
+}
